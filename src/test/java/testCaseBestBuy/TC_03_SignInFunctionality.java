7651ccc0783ab27060f7c167f58e7047489a8abb
Basil Ahamed <sbasil.ahamed@gmail.com>
2023-10-25 11:21:56 +0530

    Update TC_03_SignInFunctionality.java

--- src/test/java/testCaseBestBuy/TC_03_SignInFunctionality.java
+++ src/test/java/testCaseBestBuy/TC_03_SignInFunctionality.java
@@ -15,7 +15,7 @@ public class TC_03_SignInFunctionality extends BaseClassBB {
 		testName = "tc_03_SignInFunctionality";
 		testDescription = "Sign-In Functionality";
 		testCategory = "Regression";
-		testAuthor = "Vinothkumar";
+		testAuthor = "Basil Ahamed";
 		sheetName="signInTestData";
 	}
 	
